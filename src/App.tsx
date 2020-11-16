9e98c5f6c221ccde67f1037398ec2250bfa87a91
Ainika <aina.kostina@gmail.com>
2020-11-17 01:13:07 +0200

    Rectangles app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Button } from './components/Button/button';
 
-function App() {
+
+type Rectangle = {
+  id: number,
+  amount: number,
+  color: string,
+};
+
+
+const ColorApp = () => {
+  const [rectangles, setRectangles] = useState<Rectangle[]>([]);
+  const [inputColor, setInputColor] = useState('');
+  const [showAmount, setShowAmount] = useState(true);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="colorApp">
+      <div className="input-wrapper">
+        <input
+          type="text"
+          className="input"
+          value={inputColor}
+          placeholder="Enter your color..."
+          onChange={(e) => setInputColor(e.target.value)}
+        />
+        <Button
+          text="Add"
+          onClick={() => {
+            if (inputColor) {
+              if (rectangles.some(({ color }) => color === inputColor)) {
+                const index = rectangles
+                  .findIndex(({ color }) => color === inputColor);
+                rectangles[index].amount += 1;
+              } else {
+                setRectangles([
+                  ...rectangles,
+                  {
+                    id: rectangles.length + 1,
+                    amount: 1,
+                    color: inputColor,
+                  }
+                ]);
+              }
+            }
+            setInputColor('');
+          }}
+        />
+      </div>
+
+      {rectangles.map(({ id, amount, color }) => {
+        return (
+          <div
+            key={id}
+            className="rectangle"
+            style={{ backgroundColor: color }}
+          >
+            {amount > 1 &&
+              <div
+                role="button"
+                tabIndex={0}
+                onClick={() => setShowAmount(false)}
+                onKeyDown={() => setShowAmount(false)}
+              >
+                <div style={{ display: showAmount ? 'block' : 'none' }}>{amount}</div>
+              </div>}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
-export default App;
+export default ColorApp;
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import styles from './button.module.scss';
+
+
+type Props = {
+  text: string,
+  onClick?: () => void
+};
+
+export const Button: FC<Props> = ({ text, onClick }) => {
+  return (
+    <div className="container">
+      <button
+        type="button"
+        onClick={onClick}
+        className={styles.button}
+      >{text}
+      </button>
+    </div>
+  );
+};
